d1e9be2a33fc00a4fb460026112b83d9b93ed580
Tuấn Sơn <127265411+sonmtph23495@users.noreply.github.com>
2023-10-23 21:17:36 +0700

    Revert "Develop"

--- src/models/user.js
+++ src/models/user.js
@@ -6,21 +6,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-
-    fullname: {
-      type: String,
-      required: true,
-    },
-    ngaysinh: {
-      type: Date,
-      required: true,
-    },
-
     fullname:{
       type: String,
       required: true,
     },
-
     email: {
       type: String,
       required: true,
@@ -30,38 +19,26 @@ const userSchema = new mongoose.Schema(
       required: true,
     },
     role: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Role",
-    },
-    status: {
       type: String,
+      default: "member",
     },
     image_url: {
       type: String,
     },
     favoriteProducts: [
       {
-        type: mongoose.Schema.Types.ObjectId,
+        type: mongoose.Types.ObjectId,
         ref: "FavoriteProduct",
       },
     ],
     addressUser: [
       {
-        type: mongoose.Schema.Types.ObjectId,
+        type: mongoose.Types.ObjectId,
         ref: "Address",
       },
     ],
   },
   { timestamps: { currentTime: () => Date.now() + 7 * 60 * 60 * 1000 }, versionKey: false }
 );
-
-userSchema.pre("save", async function (next) {
-  if (!this.role) {
-    const Role = mongoose.model("Role");
-    const defaultRole = await Role.findOne({ role_name: "user" });
-    this.role = defaultRole._id;
-  }
-  next();
-});
 
 export default mongoose.model("User", userSchema);
